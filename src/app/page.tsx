874d50406943fdd5e94c17b950ab7ee7dd8438f7
umit.han <umit.han@tapu.com>
2025-08-19 21:10:49 +0200

    feat: create project

---
+++ src/app/page.tsx
@@ -0,0 +1,84 @@
+import banner from "@/assets/banner.jpg";
+import Product from "@/components/Product";
+import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
+import { getWixServerClient } from "@/lib/wix-client.server";
+import { getCollectionBySlug } from "@/wix-api/collections";
+import { queryProducts } from "@/wix-api/products";
+import { ArrowRight } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { Suspense } from "react";
+
+export default function Home() {
+  return (
+    <main className="mx-auto max-w-7xl space-y-10 px-5 py-10">
+      <div className="flex items-center bg-secondary md:h-96">
+        <div className="space-y-7 p-10 text-center md:w-1/2">
+          <h1 className="text-3xl font-bold md:text-4xl">
+            Fill the void in your heart
+          </h1>
+          <p>
+            Tough day? Credit card maxed out? Buy some expensive stuff and
+            become happy again!
+          </p>
+          <Button asChild>
+            <Link href="/shop">
+              Shop Now <ArrowRight className="ml-2 size-5" />
+            </Link>
+          </Button>
+        </div>
+        <div className="relative hidden h-full w-1/2 md:block">
+          <Image
+            src={banner}
+            alt="Flow Shop banner"
+            className="h-full object-cover"
+          />
+          <div className="absolute inset-0 bg-gradient-to-r from-secondary via-transparent to-transparent" />
+        </div>
+      </div>
+      <Suspense fallback={<LoadingSkeleton />}>
+        <FeaturedProducts />
+      </Suspense>
+    </main>
+  );
+}
+
+async function FeaturedProducts() {
+  const wixClient = getWixServerClient();
+
+  const collection = await getCollectionBySlug(wixClient, "featured-products");
+
+  if (!collection?._id) {
+    return null;
+  }
+
+  const featuredProducts = await queryProducts(wixClient, {
+    collectionIds: collection._id,
+  });
+
+  if (!featuredProducts.items.length) {
+    return null;
+  }
+
+  return (
+    <div className="space-y-5">
+      <h2 className="text-2xl font-bold">Featured Products</h2>
+      <div className="flex grid-cols-2 flex-col gap-5 sm:grid md:grid-cols-3 lg:grid-cols-4">
+        {featuredProducts.items.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function LoadingSkeleton() {
+  return (
+    <div className="flex grid-cols-2 flex-col gap-5 pt-12 sm:grid md:grid-cols-3 lg:grid-cols-4">
+      {Array.from({ length: 8 }).map((_, i) => (
+        <Skeleton key={i} className="h-[26rem] w-full" />
+      ))}
+    </div>
+  );
+}
---
+++ src/lib/utils.ts
@@ -0,0 +1,52 @@
+import { products } from "@wix/stores";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import resolveConfig from "tailwindcss/resolveConfig";
+import tailwindConfig from "../../tailwind.config";
+
+export const twConfig = resolveConfig(tailwindConfig);
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export async function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+export function formatCurrency(
+  price: number | string = 0,
+  currency: string = "USD",
+) {
+  return Intl.NumberFormat("en", { style: "currency", currency }).format(
+    Number(price),
+  );
+}
+
+export function findVariant(
+  product: products.Product,
+  selectedOptions: Record<string, string>,
+) {
+  if (!product.manageVariants) return null;
+
+  return (
+    product.variants?.find((variant) => {
+      return Object.entries(selectedOptions).every(
+        ([key, value]) => variant.choices?.[key] === value,
+      );
+    }) || null
+  );
+}
+
+export function checkInStock(
+  product: products.Product,
+  selectedOptions: Record<string, string>,
+) {
+  const variant = findVariant(product, selectedOptions);
+
+  return variant
+    ? variant.stock?.quantity !== 0 && variant.stock?.inStock
+    : product.stock?.inventoryStatus === products.InventoryStatus.IN_STOCK ||
+        product.stock?.inventoryStatus ===
+          products.InventoryStatus.PARTIALLY_OUT_OF_STOCK;
+}
