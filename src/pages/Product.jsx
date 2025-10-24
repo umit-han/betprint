0389593944ef3528efbe732e2cf3d9b1de587dc2
umit.han <umit.han@tapu.com>
2025-10-24 16:08:46 +0200

    fix: added euro sembol in product detail

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -58,7 +58,10 @@ const Product = () => {
               <img src={assets.star_dull_icon} alt="" className="w-3 5" />
               <p className='pl-2'>(122)</p>
           </div>
-          <p className='mt-5 text-3xl font-medium'>{currency}{productData.price}</p>
+          <div className='flex items-center gap-2'>
+            <p className='mt-5 text-3xl font-medium'>{currency}{productData.price}</p> 
+            <p className='mt-5 text-2xl'>€</p>
+          </div>
           <p className='mt-5 text-gray-500 md:w-4/5 mb-5'>{productData.description}</p>
           <div className='flex flex-col gap-4 my-8'>
               <p>Select Currency</p>
